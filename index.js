c564a173c629d71bfc7e023f2bc581206ff72c32
Miguel Escobar <mae2136@gmail.com>
2022-04-03 23:22:43 -0500

    V 0.16 - Add all class dependencies to index.js file.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 const fs = require(`fs`);
+const inquirer = require("inquirer");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
